7b3052b45727d2d1260945a17826f5195a9b8b74
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2025-10-29 16:53:11 +0100

    Update index.blade.php

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -233,4 +233,21 @@
 
 @section('scripts')
 @include('partials.toast')
+<script>
+    // Función para mostrar/ocultar contraseña
+    function togglePassword() {
+        const passwordInput = document.getElementById('contrasena');
+        const toggleIcon = document.getElementById('password-toggle-icon');
+        
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            toggleIcon.classList.remove('fa-eye');
+            toggleIcon.classList.add('fa-eye-slash');
+        } else {
+            passwordInput.type = 'password';
+            toggleIcon.classList.remove('fa-eye-slash');
+            toggleIcon.classList.add('fa-eye');
+        }
+    }
+</script>
 @endsection
